de2ba0be1bc2e88217d7661fd37a56f7ec181650
PawelPaziewski <lrpapawel@gmail.com>
2020-06-29 19:15:29 +0200

    Add all project files

---
+++ var/cache/dev/twig/fa/fa6c800f3a4b57543b6a2e271eadae8dd1cc16144c097bc72e03c8b711a937be.php
@@ -0,0 +1,163 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* add/index.html.twig */
+class __TwigTemplate_54cbbefac2089b7104602e80b2edab36f50890b32dca28c4233c2ec052d496e5 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+            'title' => [$this, 'block_title'],
+            'body' => [$this, 'block_body'],
+        ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "add/index.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "add/index.html.twig"));
+
+        $this->parent = $this->loadTemplate("base.html.twig", "add/index.html.twig", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 3
+    public function block_title($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        $this->displayParentBlock("title", $context, $blocks);
+        echo " Dodaj";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 5
+    public function block_body($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        // line 6
+        echo "    <div class=\"container\">
+        <h1 class=\"h3 mb-3 font-weight-normal\">Dodaj komiks</h1>
+        ";
+        // line 8
+        $this->env->getRuntime("Symfony\\Component\\Form\\FormRenderer")->setTheme((isset($context["addComic"]) || array_key_exists("addComic", $context) ? $context["addComic"] : (function () { throw new RuntimeError('Variable "addComic" does not exist.', 8, $this->source); })()), [0 => "form/bootstrap_4_layout.html.twig"], true);
+        // line 9
+        echo "        ";
+        echo         $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->renderBlock((isset($context["addComic"]) || array_key_exists("addComic", $context) ? $context["addComic"] : (function () { throw new RuntimeError('Variable "addComic" does not exist.', 9, $this->source); })()), 'form_start');
+        echo "
+        ";
+        // line 10
+        echo $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock(twig_get_attribute($this->env, $this->source, (isset($context["addComic"]) || array_key_exists("addComic", $context) ? $context["addComic"] : (function () { throw new RuntimeError('Variable "addComic" does not exist.', 10, $this->source); })()), "title", [], "any", false, false, false, 10), 'row');
+        echo "
+        ";
+        // line 11
+        echo $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock(twig_get_attribute($this->env, $this->source, (isset($context["addComic"]) || array_key_exists("addComic", $context) ? $context["addComic"] : (function () { throw new RuntimeError('Variable "addComic" does not exist.', 11, $this->source); })()), "filename", [], "any", false, false, false, 11), 'row');
+        echo "
+        <button class=\"btn btn-lg btn-primary\" type=\"submit\">
+            Wyślij
+        </button>
+        ";
+        // line 15
+        echo         $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->renderBlock((isset($context["addComic"]) || array_key_exists("addComic", $context) ? $context["addComic"] : (function () { throw new RuntimeError('Variable "addComic" does not exist.', 15, $this->source); })()), 'form_end');
+        echo "
+
+
+    </div>
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "add/index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  111 => 15,  104 => 11,  100 => 10,  95 => 9,  93 => 8,  89 => 6,  79 => 5,  59 => 3,  36 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{% extends 'base.html.twig' %}
+
+{% block title %}{{ parent() }} Dodaj{% endblock %}
+
+{% block body %}
+    <div class=\"container\">
+        <h1 class=\"h3 mb-3 font-weight-normal\">Dodaj komiks</h1>
+        {% form_theme addComic 'form/bootstrap_4_layout.html.twig' %}
+        {{ form_start(addComic) }}
+        {{ form_row(addComic.title) }}
+        {{ form_row(addComic.filename) }}
+        <button class=\"btn btn-lg btn-primary\" type=\"submit\">
+            Wyślij
+        </button>
+        {{ form_end(addComic) }}
+
+
+    </div>
+{% endblock %}
+", "add/index.html.twig", "C:\\xampp\\htdocs\\templates\\add\\index.html.twig");
+    }
+}
---
+++ var/cache/dev/url_matching_routes.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+        '/_profiler' => [[['_route' => '_profiler_home', '_controller' => 'web_profiler.controller.profiler::homeAction'], null, null, null, true, false, null]],
+        '/_profiler/search' => [[['_route' => '_profiler_search', '_controller' => 'web_profiler.controller.profiler::searchAction'], null, null, null, false, false, null]],
+        '/_profiler/search_bar' => [[['_route' => '_profiler_search_bar', '_controller' => 'web_profiler.controller.profiler::searchBarAction'], null, null, null, false, false, null]],
+        '/_profiler/phpinfo' => [[['_route' => '_profiler_phpinfo', '_controller' => 'web_profiler.controller.profiler::phpinfoAction'], null, null, null, false, false, null]],
+        '/_profiler/open' => [[['_route' => '_profiler_open_file', '_controller' => 'web_profiler.controller.profiler::openAction'], null, null, null, false, false, null]],
+        '/add' => [[['_route' => 'add', '_controller' => 'App\\Controller\\AddController::index'], null, null, null, false, false, null]],
+        '/index' => [[['_route' => 'index', '_controller' => 'App\\Controller\\IndexController::index'], null, null, null, false, false, null]],
+        '/' => [[['_route' => 'in', '_controller' => 'App\\Controller\\IndexController::index'], null, null, null, false, false, null]],
+        '/my/comics' => [[['_route' => 'my_comics', '_controller' => 'App\\Controller\\MyComicsController::index'], null, null, null, false, false, null]],
+        '/random' => [[['_route' => 'random', '_controller' => 'App\\Controller\\RandomController::index'], null, null, null, false, false, null]],
+        '/register' => [[['_route' => 'app_register', '_controller' => 'App\\Controller\\RegistrationController::register'], null, null, null, false, false, null]],
+        '/login' => [[['_route' => 'app_login', '_controller' => 'App\\Controller\\SecurityController::login'], null, null, null, false, false, null]],
+        '/logout' => [[['_route' => 'app_logout', '_controller' => 'App\\Controller\\SecurityController::logout'], null, null, null, false, false, null]],
+        '/top' => [[['_route' => 'top', '_controller' => 'App\\Controller\\TopController::index'], null, null, null, false, false, null]],
+    ],
+    [ // $regexpList
+        0 => '{^(?'
+                .'|/_(?'
+                    .'|error/(\\d+)(?:\\.([^/]++))?(*:38)'
+                    .'|wdt/([^/]++)(*:57)'
+                    .'|profiler/([^/]++)(?'
+                        .'|/(?'
+                            .'|search/results(*:102)'
+                            .'|router(*:116)'
+                            .'|exception(?'
+                                .'|(*:136)'
+                                .'|\\.css(*:149)'
+                            .')'
+                        .')'
+                        .'|(*:159)'
+                    .')'
+                .')'
+                .'|/like/([^/]++)(*:183)'
+                .'|/unlike/([^/]++)(*:207)'
+                .'|/delete/([^/]++)(*:231)'
+            .')/?$}sDu',
+    ],
+    [ // $dynamicRoutes
+        38 => [[['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null]],
+        57 => [[['_route' => '_wdt', '_controller' => 'web_profiler.controller.profiler::toolbarAction'], ['token'], null, null, false, true, null]],
+        102 => [[['_route' => '_profiler_search_results', '_controller' => 'web_profiler.controller.profiler::searchResultsAction'], ['token'], null, null, false, false, null]],
+        116 => [[['_route' => '_profiler_router', '_controller' => 'web_profiler.controller.router::panelAction'], ['token'], null, null, false, false, null]],
+        136 => [[['_route' => '_profiler_exception', '_controller' => 'web_profiler.controller.exception_panel::body'], ['token'], null, null, false, false, null]],
+        149 => [[['_route' => '_profiler_exception_css', '_controller' => 'web_profiler.controller.exception_panel::stylesheet'], ['token'], null, null, false, false, null]],
+        159 => [[['_route' => '_profiler', '_controller' => 'web_profiler.controller.profiler::panelAction'], ['token'], null, null, false, true, null]],
+        183 => [[['_route' => 'like', '_controller' => 'App\\Controller\\TopController::like'], ['id'], null, null, false, true, null]],
+        207 => [[['_route' => 'unlike', '_controller' => 'App\\Controller\\TopController::unlike'], ['id'], null, null, false, true, null]],
+        231 => [
+            [['_route' => 'delete', '_controller' => 'App\\Controller\\TopController::delete'], ['id'], null, null, false, true, null],
+            [null, null, null, null, false, false, 0],
+        ],
+    ],
+    null, // $checkCondition
+];
